984023071b9cf8a0cf1978e90591540f4797e030
Alessandro Ranellucci <alessandro@pintle.it>
2018-08-07 12:17:36 +0200

    Initial import of git-multipull.sh

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # git-multipull
+
+This script fetches multiple remote git repositories whose master branches are supposed to be identical. It checks that they are actually aligned, and then performs a merge of their history to the local master branch.
+
+The purpose of such a pedantic tool is to enable secure, decentralised distribution of authoritative data. Many git hosting providers keep audit logs of force-pushes, so it's almost impossible to forge the history without leaving traces. In case a single repository is compromised, `git fetch` will complain and this script will refuse to pull the changes anyway.
---
+++ git-multipull.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+
+# Alessandro Ranellucci
+# Team per la Trasformazione Digitale - Presidenza del Consiglio dei Ministri
+
+# Loop through repos, fetch them and compare their tips.
+echo "Fetching remotes..."
+tip=""
+for repo in `cat repos.txt`; do
+    # Strip https:// from the URLs in order to get a valid refspec name
+    branch=${repo/#https:\/\//}
+    
+    # Fetch the repository to its own local branch
+    echo git fetch $repo master:$branch
+    git fetch $repo master:$branch
+    if [ $? -ne 0 ]; then
+        echo "FATAL: Fetch failed. Please check network status and authenticity of the repositories."
+        exit $?
+    fi
+    
+    # Compare the tip of this repository with the previous one.
+    if [ "$tip" == "" ]; then
+        tip=`git rev-parse $branch`
+    elif [ "$tip" != `git rev-parse $branch` ]; then
+        echo "FATAL: Security check failed: remotes tips don't match. Please check authenticity of the repositories."
+        exit 1
+    fi
+done
+echo "Security check succeeded: remotes are aligned ($tip)."
+
+echo "Checking out files..."
+echo git merge $tip
+git merge $tip
